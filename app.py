f6f1e3532800cf475c551cfb62d68538f3b3cc75
rifkegribenes <rifkegribenes@gmail.com>
2019-01-27 22:12:39 -0800

    GET endpoint

--- app.py
+++ app.py
@@ -1,9 +1,48 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello world"
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>') # 'http://127.0.0.1:500/store/some_name'
+def get_store(name):
+    pass
+
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+
+
+# POST /store/<string:name>/item {name: price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
+
 
 app.run(port=5000)
